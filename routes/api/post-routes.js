49058946c503b435df632f0491aef40d6e18781f
felipezintzun <felipezintzun@gmail.com>
2022-02-12 19:52:00 -0600

    still unable to test vote route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -65,7 +65,12 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+      })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => res.json(err));
 });
 
 // updates post
